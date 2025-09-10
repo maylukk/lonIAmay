bbae7b5e2ff9f6e7354bd26e571a8cd54763c52e
maylukk <142520679+maylukk@users.noreply.github.com>
2025-09-10 20:58:24 -0300

    ok

---
+++ script.js
@@ -0,0 +1,23 @@
+const caixaPrincipal=document.querySelector(".caxa-pricipal");
+const caixaPerguntas=document.querySelector(".caixa-perguntas");
+const caixaAlternativas=document.querySelector(".caixa-alternativas");
+const caixaResultado=document.querySelector(".caixa-resultado");
+const textoResultado=document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+       enunciado:"Assim que saiu do colégio você se depara com uma nova tecnologia, um chat que consegue responder todas as duvidas que uma pessoa pode ter, ele tambem gera imagens e audios. Qual é o seu primeiro pensamento?",
+       alrernativas:[
+        {
+            texto: "Isso é assustador!",
+            afirmacao:"No inicio, eu ficaria com medo do que essa tecnologia pode fazer."
+               
+            
+        },
+        {
+            
+        }
+
+       ]
+    }
+]
